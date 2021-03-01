be26a297e6d72f3cac08afccd96b1b8e5f6b33c8
Leo-Xee <jang.xmin@gmail.com>
2021-03-01 23:10:12 +0900

    add movie data API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movie: [],
+  }
+
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    console.log(movies);
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading} = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : "We are Ready!!" }
+      </div>
+    );
+  }
 }
+
 
 export default App;
