8d3f5a2a195c75b48302b5aa3fe62c8e91a093c7
Leo-Xee <jang.xmin@gmail.com>
2021-03-03 14:55:56 +0900

    add details

--- src/routers/About.js
+++ src/routers/About.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h1 className="about-title">About</h1>
+      <p className="about-content">This Page is for Movie introduction. :)</p>
+    </div>
+  );
 }
 
 export default About;
--- src/routers/Detail.js
+++ src/routers/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -10,7 +11,25 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <div>{location.state.title}</div>;
+      return (
+        <div className="movie-details">
+          <img
+            className="movie-details-poster"
+            src={location.state.poster}
+            alt={location.state.title}
+          />
+          <div className="movie-details-data">
+            <h2 className="movie-details-title">{location.state.title}</h2>
+            <div className="movie-details-year">{location.state.year}</div>
+            <ul className="movie-details-genres">
+              {location.state.genres.map((genre, idx) => {
+                return <li key={idx}>{genre}</li>;
+              })}
+            </ul>
+            <p className="movie-details-summary">{location.state.summary}</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
