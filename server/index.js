029da90e036f29e0f2dc23990a4712ef4aacdc5a
Jangmin Lee <jang.xmin@gmail.com>
2021-12-08 14:43:34 +0900

    [ADD] User 모델 추가

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const URI = process.env.MONGO_URI;
 mongoose
