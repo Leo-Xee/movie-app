631a729b487362de787e355df8941f58df1e13e3
Jangmin Lee <jang.xmin@gmail.com>
2021-12-08 13:52:22 +0900

    [ADD] MongoDB 연결

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT;
+
+const URI = process.env.MONGO_URI;
+mongoose
+  .connect(URI)
+  .then(() => console.log('MongoDB Connected!!'))
+  .catch((e) => console.log(e));
+
+app.get('/', (req, res) => {
+  res.send('Hello world!!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
