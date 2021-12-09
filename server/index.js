8d896175432045fac8576bfa9f1a5ced6316800f
Jangmin Lee <jang.xmin@gmail.com>
2021-12-09 11:12:34 +0900

    [ADD] signUp API 추가
    - bcrypt로 password만 암호화한 후 DB에 저장

--- server/index.js
+++ server/index.js
@@ -2,8 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
+const { User } = require('./models/User');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const URI = process.env.MONGO_URI;
 mongoose
@@ -14,6 +19,22 @@ mongoose
 app.get('/', (req, res) => {
   res.send('Hello world!!');
 });
+
+// 회원가입
+app.post('/api/users/signUp', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err) => {
+    if (err) return res.json({ sucess: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+// 로그인
+
+// 로그아웃
+
+// 인증
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -29,6 +32,25 @@ const userSchema = new mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// User가 save되기 전에 비밀번호 암호화
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model('User', userSchema);
